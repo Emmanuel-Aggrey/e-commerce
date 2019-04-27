5f6f0452d5c24ec7b2bee9bd46d3c07248589788
= <=>
2019-04-27 01:50:26 +0000

    fix database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -85,12 +85,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
